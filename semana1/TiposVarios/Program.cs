a15a5f498535ffd0b23e93df2096f4fc1a4a9e3e
Gerson Monteiro <gersonn.m270@gmail.com>
2023-09-30 12:41:06 +0100

    TiposVarios

---
+++ semana1/TiposVarios/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//
+
+            int a=22;
+            sbyte b=-22 ;
+            short c=-30000;
+            long k=300000000 ;
+
+            byte d=222 ;
+            ushort e= 60022;
+            uint f=1000000 ;
+            ulong g=7000000 ;
+
+            float h=1.0522f ;
+            double i=5.22 ;
+            decimal j=22.0501m ;
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(k);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+
+        }
+    }
+}
