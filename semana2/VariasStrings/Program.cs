4abb025f8382c0178077efbe7ed43b1d6a4acf22
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-02 09:20:24 +0100

    VariasStrings

--- semana2/VariasStrings/Program.cs
+++ semana2/VariasStrings/Program.cs
@@ -6,8 +6,18 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-           string animal="Macaco"; 
-           string m="Um tab \t e um simbolo de copyright \u00A9 "
+           string s="uma string"; 
+           string m="""uma string entre aspas""";
+           string p=@"\uma string sem aspas";
+           string n="\u00A9 e \u2665";
+           string c=@"\u00A9 e \u2665 sem UTF";
+
+           con
+
+           
+
+
+           
         }
     }
 }
