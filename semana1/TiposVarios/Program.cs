193e5d4459320f1a35610c6390bfdfc13b8418cd
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-02 08:32:27 +0100

    sdicio nar solução para semana2

--- semana1/TiposVarios/Program.cs
+++ semana1/TiposVarios/Program.cs
@@ -6,23 +6,40 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
-//
-
+            /// <summary>
+            /// são criadas 8 variaveis do tipo de numeros iteiro e reais  simples 
+            /// </summary>
+            //nº inteiros signed
             int a=22;
-            sbyte b=-22 ;
+            sbyte b=-22;
             short c=-30000;
-            long k=300000000 ;
-
-            byte d=222 ;
+            long k=300000000L;
+            //nº inteiros unsigned
+            byte d=222;
             ushort e= 60022;
-            uint f=1000000 ;
-            ulong g=7000000 ;
+            uint f=1000000U;
+            ulong g=7000000UL;
+            //nº reais
+            float h=22.05f;
+            double i=5.308;
+            decimal j=99.99m;
 
-            float h=1.0522f ;
-            double i=5.22 ;
-            decimal j=22.0501m ;
+           char copyrrightsymbol='\u00A9';
+           char newLine = '\u000A';
+           
+           int n1=10;
+           int n2=20;
+           int n3=30;
+           string resultado="falso";
 
+           bool nmaior=n2>n1;
 
+           if(n3 < 100){
+               resultado="verdade";
+              }
+           
+
+            //mostra no ecra os numeros atribuido a cada uma das variaveis            
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
@@ -35,6 +52,16 @@ namespace TiposVarios
             Console.WriteLine(i);
             Console.WriteLine(j);
 
+            Console.WriteLine(copyrrightsymbol);
+            Console.WriteLine(newLine);
+
+            Console.WriteLine(nmaior);
+             Console.WriteLine("resultado: {0}",resultado);
+
+            
+
+       
+            
 
         }
     }
