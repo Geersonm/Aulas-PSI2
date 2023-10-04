64e4fb9d36a36cdb23775d828539c9da953eb089
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-04 09:34:35 +0100

    cilindro/

---
+++ semana2/cilindro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double r;
+            double p =3.1415926;
+            double v=p* (r*r)*a;
+            double sa=2*p*r(r+a);
+
+            Console. ("Introduaza a  altura");
+            string=Console.ReadLine(a);
+            Console.WriteLine("Introduza o raio");
+            string=Console.ReadLine(r);
+
+
+            Console.WriteLine(v);
+            Console.WriteLine(sa);
+        }
+    }
+}
