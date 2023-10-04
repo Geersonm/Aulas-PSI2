444ebdcc2ad9732c97ef88b1de32db2fb1421def
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-04 08:41:08 +0100

    IncDec/IncDec

---
+++ semana2/IncDec/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = -5, c;
+                c = a++ + b++;
+
+            int d = 2, e = -5, f;
+                f = --d + --e;
+
+                Console.WriteLine(c);
+                Console.WriteLine(f);
+
+
+
+
+        }
+    }
+}
--- semana2/VariasStrings/Program.cs
+++ semana2/VariasStrings/Program.cs
@@ -6,18 +6,51 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-           string s="uma string"; 
-           string m="""uma string entre aspas""";
-           string p=@"\uma string sem aspas";
-           string n="\u00A9 e \u2665";
-           string c=@"\u00A9 e \u2665 sem UTF";
+            int x = 10, y = 5;
+            char c = 'a';
 
-           Console.WriteLine(s);
+            double dd = 0.12345;
+            int ii = 18;
 
-           
+            string s = "uma string";
+            string m = """uma string entre aspas""";
+            string p = @"\uma string sem aspas";
+            string n = "\u00A9 e \u2665";
+            string c = @"\u00A9 e \u2665 sem UTF";
+
+            string cs = "string" + "conectada" + 2;
+            string cr = s + m + x;
+
+            string si = $"{s}interpolada";
+
+            string j = "a" + 2;
+            string d = "b" + x;
+
+            string s = String.Format(“Arg { 0}
+            e { 1}”, x, y);
+            string s = String.Format(“Arg { 1}
+            e { 0}”, 5, 10);
+            string s = String.Format(“Olá { 0}, No. { 1}”, “mundo”, 3);
 
 
-           
+            Console.WriteLine(s);
+            Console.WriteLine(m);
+            Console.WriteLine(p);
+            Console.WriteLine(n);
+                                                                 Console.WriteLine(c);
+
+            Console.WriteLine(cs);
+
+
+            Console.WriteLine(“Valor de x é { 0}”, x);
+
+            Console.WriteLine($"(dd:f2)");
+            Console.WriteLine($"(dd:p1)");
+            Console.WriteLine($"(ii:x)");
+            Console.WriteLine($"(ii:c)");
+
+
+
         }
     }
 }
