d9af6cbe07bb8a53da1b55c705a3d347b4bcf1d5
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-06 09:13:03 +0100

    cilindro

--- semana2/cilindro/Program.cs
+++ semana2/cilindro/Program.cs
@@ -6,27 +6,27 @@ namespace cilindro
     {
         static void Main(string[] args)
         {
-            double a,r,v,sa;
-            double p = 3.1415926;
-            string altura,raio;
+            double a, r, v, sa;
         
+            string altura, raio;
 
-            Console.WriteLine ("Introduaza a  altura");
-            altura= Console.ReadLine();
+
+            Console.WriteLine("Introduaza a  altura");
+            altura = Console.ReadLine();
             Console.WriteLine("Introduza o raio");
-            raio=Console.ReadLine();
+            raio = Console.ReadLine();
 
 
-            a=double.Parse(altura);
-            r=double.Parse(raio);
+            a = double.Parse(altura);
+            r = double.Parse(raio);
 
 
-            v = p * (r * r) * a;
-            sa = 2 * p * r* (r + a);
+            v = Math.PI * Math.Pow(r,2) * a;
+            sa = 2 * Math.PI * r * (r + a);
 
-
+            Console.WriteLine($"cilindro com {altura} de altura  e {raio} de raio tem:\n");
             Console.WriteLine($"volume {v} \n");
-            Console.WriteLine($"cilindro com {altura} e {raio} tem:\n");
+
             Console.WriteLine($"Area da superficie {sa} \n");
         }
     }
