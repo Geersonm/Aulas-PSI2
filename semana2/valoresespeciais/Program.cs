6862cd0747cd451da29ae6644ede0cb11bf4bd12
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-06 10:23:25 +0100

    valoresespeciais

--- semana2/valoresespeciais/Program.cs
+++ semana2/valoresespeciais/Program.cs
@@ -6,7 +6,38 @@ namespace valoresespeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+
+            Console.WriteLine();
+
+            Console.WriteLine(double.PositiveInfinity);
+            Console.WriteLine(double.NegativeInfinity);
+            Console.WriteLine(double.NaN);
+
+            Console.WriteLine();
+                      
+            Console.WriteLine(float.PositiveInfinity);
+            Console.WriteLine(float.NegativeInfinity);
+            Console.WriteLine(float.NaN);
+
+
+
         }
     }
 }
