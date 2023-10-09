45572c2ed3c4587b46fd68cba4a70d7f568d3c82
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-09 09:03:42 +0100

    skynet/

---
+++ semana2/skynet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace skynet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p;
+
+        for(int i=0;i<3;i++){
+                Console.WriteLine("Faz uma pergunta");
+                p=Console.ReadLine();
+
+                if(p=="Que horas são?"){
+                    Console.WriteLine("Falta pouco para daqui a pouco");
+                }else if(p=="Qual é o melhor jogdor do mundo?"){
+                    Console.WriteLine("É o CJ");
+                }else if(p=="Eu ssou negrão?"){
+                    Console.WriteLine("Sim");
+                }else{
+                    Console.WriteLine("Não sei");
+                }
+            }
+        }
+    }
+}
