9d53996809cfa7d70447668de06b78ea48e9e1b3
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-11 22:52:56 +0100

    piramide

---
+++ semana3/piramide/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace piramide
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string nu;
+      int n;
+
+      Console.WriteLine("\nIntroduza um numero: ");
+      nu = Console.ReadLine();
+      n = Int32.Parse(nu);
+
+      if (n >= 2)
+      {
+        for (int l = 1; l <= n; l++)
+        {
+          for (int e = 1; e <= n - l; e++)
+          {
+            Console.Write(" ");
+
+          }
+
+          for (int a = 1; a <=2*l-1; a++)
+          {
+            Console.Write("*");
+            
+          }
+          Console.WriteLine();
+        }
+
+
+      }
+    }
+  }
+}
