d1ee2129d3a7c293d8eed21b4df0af02af5df932
Gerson Monteiro <gersonn.m270@gmail.com>
2023-10-16 08:27:56 +0100

    BiDimArray

---
+++ semana3/BiDimArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BiDimArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string u, uu;
+            float h, v;
+            float[,] hv;
+            double l ;
+
+            Console.Write("\nIntroduza a dimenssão horizontal");
+            u = Console.ReadLine();
+            h = float.Parse(u);
+
+            Console.Write("\nIntroduza a dimenssão vertical");
+            uu = Console.ReadLine();
+            v = float.Parse(uu);
+
+
+            
+        }
+    }
+}
