8c33a5b7865173077d9007c6db3bdaf7b057b673
Gerson Monteiro <gersonn.m270@gmail.com>
2024-01-10 09:32:05 +0000

    Escrever02

---
+++ semana9/Escrever02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+
+namespace Escrever02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        // Caminho universal para ficheiro no ambiente de trabalho
+        private static string caminho = Path.Combine(
+          Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+          "input.txt");
+
+        static void Main(string[] args)
+        {
+            // Variável para guardar input do utilizador
+            string input;
+
+            // Instância de StreamWriter para escrever no ficheiro
+            StreamWriter sw = new StreamWriter(caminho);
+
+            Console.Write("Escreve várias linhas de texto para guardar num ficheiro");
+            Console.Write("\nPressiona a tecla ENTER quando quiseres terminar\n");
+
+            // Ciclo para ler input do utilizador e escrever no ficheiro
+            // enquanto não for inserida uma string vazia
+            do
+            {
+                input = Console.ReadLine();
+
+                if (input != "")
+                    sw.WriteLine(input);
+            }
+            while (input != "");
+
+            // Fechar ficheiro
+            sw.Close();
+        }
+    }
+}
+
+        }
+
+
