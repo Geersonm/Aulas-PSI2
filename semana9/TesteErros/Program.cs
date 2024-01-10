022a0a26b811550a8b67d3faef24ce610a7dd61c
Gerson Monteiro <gersonn.m270@gmail.com>
2024-01-10 09:23:17 +0000

    Testar erros

---
+++ semana9/TesteErros/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TesteErros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Introduza um numero");
+            try
+            {
+                i = int.Parse(Console.ReadLine());
+            }
+
+            catch (FormatException e)
+            {
+                // Caso não tenha sido inserido um número inteiro
+                Console.WriteLine("Error:O número não é do tipo inteiro");
+            }
+            catch (OverflowException e)
+            {
+                // Caso tenha sido inserido um número inteiro demasiado grande
+                Console.WriteLine("Error:O número é muito grande");
+            }
+            catch (Exception e)
+            {
+                // Ocorreu um erro desconhecido
+                Console.WriteLine("Error: Auto destruição ativada");
+            }
+            finally
+            {
+                Console.WriteLine("Quem ler esta menssagem é... ");
+            }
+
+
+
+
+
+        }
+    }
+}
