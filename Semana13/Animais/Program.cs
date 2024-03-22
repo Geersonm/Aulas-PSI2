e46542c6d72915ecfac7cf35db80aa9088608cee
Gerson Monteiro <gersonn.m270@gmail.com>
2024-03-22 09:42:07 +0000

    semana13

---
+++ Semana13/Animais/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Animais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
